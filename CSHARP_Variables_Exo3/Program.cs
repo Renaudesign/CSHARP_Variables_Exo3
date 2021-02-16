409b0dab2b735832297ab3edc1a7b4aae56cf0ed
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 11:08:04 +0100

    create repo

---
+++ CSHARP_Variables_Exo3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSHARP_Variables_Exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1;
+            string value2;
+
+            Console.WriteLine("Choose a value");
+            value1 = Console.ReadLine();
+            Console.WriteLine("You entered '{0}'", value1);
+
+            Console.WriteLine("Choose another value");
+            value2 = Console.ReadLine();
+            Console.WriteLine("You entered '{0}'", value2);
+        }
+    }
+}

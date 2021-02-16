9e4b9f0718e25fe06269f4701bb4d9323b4237a4
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 16:21:43 +0100

    correction exo3

--- CSHARP_Variables_Exo3/Program.cs
+++ CSHARP_Variables_Exo3/Program.cs
@@ -9,14 +9,14 @@ namespace CSHARP_Variables_Exo3
             Console.WriteLine("Choose a value");
             int value1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"You entered '{value1}', '{value1}' + 33 equals {value1+33}");
-            value1 = value1 + 33;
+            float sum = value1;
 
             Console.WriteLine("Choose another value");
             int value2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"You entered '{value2}', '{value2++}'  + 1 equals '{value2}'");
-            value2 = value2++;
 
-            Console.WriteLine($"{value1} divided by {value2} equals {value1 / value2}");
+            float division = sum / value2;
+            Console.WriteLine($"{value1} divided by {value2} equals {division}");
 
         }
     }
